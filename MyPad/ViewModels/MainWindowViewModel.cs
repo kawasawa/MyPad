b320b8b43dae2b8ca7fbf9a4b7cfb1f6f6b1a87a
Kazuki Awasawa <16559662+kawasawa@users.noreply.github.com>
2021-01-23 00:39:49 +0900

    [refactor] フォルダ構成を変更

--- MyPad/ViewModels/MainWindowViewModel.cs
+++ MyPad/ViewModels/MainWindowViewModel.cs
@@ -929,5 +929,44 @@ namespace MyPad.ViewModels
         }
 
         #endregion
+
+        private static class CommonDialogHelper
+        {
+            public static string CreateFileFilter(SyntaxService syntaxService)
+            {
+                return string.Join("|",
+                    new[] { $"{Resources.Label_AllFiles}|*.*" }
+                    .Concat(syntaxService.Definitions.Values.Select(d => $"{d.Name}|{string.Join(";", d.Extensions)}")));
+            }
+
+            public static CommonFileDialogComboBox ConvertToComboBox(Encoding defaultEncoding)
+            {
+                var comboBox = new CommonFileDialogComboBox();
+                var encodings = Constants.ENCODINGS;
+                for (var i = 0; i < encodings.Count(); i++)
+                {
+                    comboBox.Items.Add(new EncodingComboBoxItem(encodings.ElementAt(i)));
+                    if (encodings.ElementAt(i) == defaultEncoding)
+                        comboBox.SelectedIndex = i;
+                }
+                return comboBox;
+            }
+
+            public class EncodingComboBoxItem : CommonFileDialogComboBoxItem
+            {
+                public Encoding Encoding { get; }
+
+                public EncodingComboBoxItem(Encoding encoding)
+                    : this(encoding, encoding?.EncodingName)
+                {
+                }
+
+                public EncodingComboBoxItem(Encoding encoding, string text)
+                    : base(text)
+                {
+                    this.Encoding = encoding;
+                }
+            }
+        }
     }
 }
