67e6eacae2366771d2be4b8a137822685c5ae8aa
Kazuki Awasawa <16559662+kawasawa@users.noreply.github.com>
2022-01-22 10:10:30 +0900

    [change] 名前空間の変更

---
+++ MyPad/Views/BindingProxy.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace MyPad.Views
+{
+    /// <summary>
+    /// データプロキシーオブジェクトを表します。
+    /// </summary>
+    public class BindingProxy : Freezable
+    {
+        /// <summary>
+        /// 保持されるデータの依存関係プロパティ
+        /// </summary>
+        public static readonly DependencyProperty DataProperty = DependencyPropertyExtensions.Register();
+
+        /// <summary>
+        /// 保持されるデータの CLR ラッパープロパティ
+        /// </summary>
+        public object Data
+        {
+            get => (object)this.GetValue(DataProperty);
+            set => this.SetValue(DataProperty, value);
+        }
+
+        /// <summary>
+        /// 新しいインスタンスを生成します。
+        /// </summary>
+        /// <returns>新しいインスタンス</returns>
+        protected override Freezable CreateInstanceCore()
+            => new BindingProxy();
+    }
+}
--- MyPad/Views/Markup/IconSourceExtension.cs
+++ MyPad/Views/Markup/IconSourceExtension.cs
@@ -3,8 +3,11 @@ using System.Linq;
 using System.Windows.Markup;
 using System.Windows.Media.Imaging;
 
-namespace MyPad.Views.Helpers
+namespace MyPad.Views.Markup
 {
+    /// <summary>
+    /// アイコンソースを解決するためのマークアップ拡張機能を提供します。
+    /// </summary>
     public class IconSourceExtension : MarkupExtension
     {
         private string _source;
@@ -14,19 +17,13 @@ namespace MyPad.Views.Helpers
             set => this._source = $"pack://application:,,,/{this.GetType().Assembly.GetName().Name};component/{(value.StartsWith("/") ? value[1..] : value)}";
         }
 
-        public IconSourceExtension()
-        {
-        }
+        public IconSourceExtension() { }
 
         public IconSourceExtension(string source)
-        {
-            this.Source = source;
-        }
+            => this.Source = source;
 
         public override object ProvideValue(IServiceProvider serviceProvider)
-        {
-            return BitmapDecoder.Create(new Uri(this.Source), BitmapCreateOptions.DelayCreation, BitmapCacheOption.OnDemand)
+            => BitmapDecoder.Create(new Uri(this.Source), BitmapCreateOptions.DelayCreation, BitmapCacheOption.OnDemand)
                 .Frames.OrderBy(f => f.Width).FirstOrDefault();
-        }
     }
 }
